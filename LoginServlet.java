0afc74c24a1bb04e329d5602ad5107fc93301343
banhbaomeden <leh17693@gmail.com>
2025-06-22 23:45:36 +0700

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author admin
+ */
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        
+        User user = UserDAO.authenticate(username, password);
+        
+        if (user != null) 
+        {
+            req.getSession().setAttribute("user", user);
+            res.sendRedirect("home.jsp");
+        } else {
+            req.setAttribute("error", "Login failed!");
+            req.getRequestDispatcher("login.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+   
+}
