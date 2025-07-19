aa1d9d80d05e04836ce2e5fd28c8163de49c27f6
banhbaomeden <leh17693@gmail.com>
2025-07-19 12:10:07 +0700

    Create ViewRequestServlet.java

---
+++ src/controller/ViewRequestServlet.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import dao.RequestDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.User;
+
+
+/**
+ *
+ * @author admin
+ */
+public class ViewRequestServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = (User) req.getSession().getAttribute("user");
+        if (user == null) {
+            resp.sendRedirect("login.jsp");
+            return;
+        }
+
+        req.setAttribute("requests", new RequestDAO().getByUser(user.getId()));
+        req.getRequestDispatcher("viewRequest.jsp").forward(req, resp);
+    }
+    
+}
