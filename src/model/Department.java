1130816fb8f3a791f9ffd85c08196479657ae1b2
banhbaomeden <leh17693@gmail.com>
2025-07-21 00:23:41 +0700

    Update Department.java

--- src/model/Department.java
+++ src/model/Department.java
@@ -1,30 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package model;
 
-public class Department {
+/**
+ *
+ * @author p14s
+ */
+public class Department implements IModel {
+
     private int id;
     private String name;
 
-    public Department() {}
+    @Override
+    public int getId() { return id; }
+    @Override
+    public void setId(int id) { this.id = id; }
 
-    public Department(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    // Getter & Setter
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    
 }
