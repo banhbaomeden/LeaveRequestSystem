221477b0dcc0791de4a590e2cd5e0701708b88d0
banhbaomeden <leh17693@gmail.com>
2025-07-21 00:19:57 +0700

    Update LoginServlet.java

--- src/controller/LoginServlet.java
+++ src/controller/LoginServlet.java
@@ -1,41 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package controller;
 
 import dao.UserDAO;
-import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.User;
 
-/**
- *
- * @author admin
- */
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        
-        User user = (User) UserDAO.authenticate(username, password);
-        
-        if (user != null) 
-        {
+
+        UserDAO userDao = new UserDAO();
+        User user = null;
+        try {
+            user = userDao.get(username, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+
+        if (user != null) {
             HttpSession session = req.getSession();
             session.setAttribute("user", user);
-            resp.getWriter().println(user.getDisplayname() + "Logged sucessfully!");
+            resp.getWriter().println(" logged successful!");
         } else {
             HttpSession session = req.getSession();
             session.setAttribute("user", null);
-            resp.getWriter().println("Login failed!");
+            resp.getWriter().println("login failed!");
         }
     }
 
@@ -43,6 +44,4 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("login.jsp").forward(req, resp);
     }
-
-   
 }
